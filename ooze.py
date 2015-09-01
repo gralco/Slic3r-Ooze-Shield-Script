61b3b46899ee5c6e2efbaaa46aa3c5f896982ab7
Eric Kuzmenko <eric.gralco@gmail.com>
2015-09-01 11:43:13 -0500

    Adjust E0 flow

--- ooze.py
+++ ooze.py
@@ -20,16 +20,20 @@ shield_line_range = []
 
 for line in list(enumerate(lines)):
   if not found_toolchange and "; Tool change from 1 to 0" in line[1]:
-    shield_line_range.append([line[0]])
+    shield_line_range.append([line[0]+1])
     found_toolchange = True
   elif not found_first_G92_E0 and found_toolchange and "G92 E0" in line[1]:
     found_toolchange = False
     found_first_G92_E0 = True
+  elif found_first_G92_E0 and ('E' in line[1]) and ('F' in line[1]) and ('Y' not in line[1]) and ('X' not in line[1]) and ('Z' not in line[1]):
+    print "FOUND ONE!\n"
+
+    length = float(line[1][line[1].index('E')+1:line[1].index(' F')])
+    flow_rate = float(line[1][line[1].index(' F')+2:])
+    lines[line[0]] = line[1][:line[1].index('E')+1] + str(3*length/4) + ' F' + str(3*flow_rate/4) + '\n'
   elif found_first_G92_E0 and "G92 E0" in line[1]:
     shield_line_range[-1].append(line[0]+2)
     found_first_G92_E0 = False
-
-file = open("Shield.gcode", 'w+')
 
 print shield_line_range
 
@@ -43,10 +47,10 @@ for i in range(len(shield_line_range)):
       height = float(line[4:].split(' F')[0]) - 0.18
       newline = line[0:line.index('Z')+1] + str(height) + line[line.index(' F'):]
       secondary_extruder_shield_code[i].append(newline)
-    elif 'E' in line and 'F' in line and not 'Y' in line and not 'X' in line and not 'Z' in line:
+    elif ('E' in line) and ('F' in line) and ('Y' not in line) and ('X' not in line) and ('Z' not in line):
       length = float(line[line.index('E')+1:line.index(' F')])
       flow_rate = float(line[line.index(' F')+2:])
-      newline = line[:line.index('E')+1] + str(3*length/4) + ' F' + str(3*flow_rate/4) + '\n'
+      newline = line[:line.index('E')+1] + str(1*length/1) + ' F' + str(1*flow_rate/1) + '\n'
       secondary_extruder_shield_code[i].append(newline)
     else:
       secondary_extruder_shield_code[i].append(line)
@@ -61,19 +65,6 @@ for line in list(enumerate(lines)):
     i+=len(secondary_extruder_shield_code[c])
     c+=1
   i+=1
-
-copy_current_chunk = True
-for i in range(len(shield_line_range)):
-  for line in lines[shield_line_range[i][0]:shield_line_range[i][1]]:
-    if 'E' in line and 'F' in line and not 'Y' in line and not 'X' in line and not 'Z' in line:
-      length = float(line[line.index('E')+1:line.index(' F')])
-      flow_rate = float(line[line.index(' F')+2:])
-      newline = line[:line.index('E')+1] + str(3*length/4) + ' F' + str(3*flow_rate/4) + '\n'
-      file.write(newline)
-    else:
-      file.write(line)
-  for line in secondary_extruder_shield_code[i]:
-    file.write(line)
 
 #Save
 lines = ''.join(lines)
